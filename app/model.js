484b43a220c53943ba0f62b5505c4d72d2c22ba4
unknown <dsin@SJV00193.intern.jordbruksverket.se>
2016-02-25 16:17:37 +0100

    Buld object tree from test result

--- app/model.js
+++ app/model.js
@@ -1,4 +1,4 @@
-exports.specs = function(testResult) {
+exports.specs = function(testResult, appendTo) {
 
   var propNames = [];
   var i = 0;
@@ -7,25 +7,53 @@ exports.specs = function(testResult) {
     propNames.push('d' + i++);
   });
 
-  return makeOjectTree(propNames, testResult);
+  return makeOjectTree(propNames, testResult, appendTo);
 };
 
-function makeOjectTree(propNames, testResult) {
+function makeOjectTree(propNames, testResult, appendTo) {
   var name;
-  var o = {};
+  var o = (appendTo != null) ? appendTo : {};
   var result = o;
 
   for (var i = 0, iLen = propNames.length; i < iLen; i++) {
     name = propNames[i];
 
     if (!o[name]) {
+      //new node
       o[name] = {
         description: testResult[i]
       };
       o = o[name];
+    } else if (o[name] && o[name].description === testResult[i]) {
+      //existing node
+      o = o[name];
+    } else {
+      //current node does not contain the same description.
+      for (var ii = 1, iiLen = 100; ii < iiLen; ii++) {
+        if (!o[name + '' + ii]) {
+          o[name + '' + ii] = {
+            description: testResult[i]
+          };
+         
+          o = o[name];
+          break;
+        }
+      }
+
     }
+
   }
 
   return result;
 }
 
+
+/*
+      for (var ii = 0, iiLen = 100; ii < iiLen; ii++) {
+        if (!o[name + ii]) {
+          o[name + ii] = {
+            description: testResult[i]
+          };
+        }        
+      }
+*/
--- test/model.js
+++ test/model.js
@@ -3,17 +3,26 @@ var assert = require('assert');
 
 var model = require("./../app/model");
 
-describe('Array', function() {
+describe('Model', function() {
 
-  describe('#indexOf()', function () {
+  describe('specs', function () {
 
-    it('should return -1 when the value is not present', function () {
+    it('should reflect test result as an object tree.', function () {
 
       var specs = model.specs(['lorum','ipsum', 'dolor']);
 
       expect(specs.d0.description).to.be.equal('lorum');
       expect(specs.d0.d1.description).to.be.equal('ipsum');
       expect(specs.d0.d1.d2.description).to.be.equal('dolor');
+
+      specs = model.specs(['lorum','ipsum', 'whatever'], specs);
+
+      expect(specs.d0.description).to.be.equal('lorum');
+      expect(specs.d0.d1.description).to.be.equal('ipsum');
+
+      expect(specs.d0.d1.d2.description).to.be.equal('dolor');
+      expect(specs.d0.d1.d21.description).to.be.equal('whatever');
+
     });
 
   });
